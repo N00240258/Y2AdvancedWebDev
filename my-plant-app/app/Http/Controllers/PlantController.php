28ca4758c6f93adbd8d8c98c9e637859b1d1cc56
N00240258 <N00240258@iadt.ie>
2025-09-16 16:22:43 +0100

    AllPlantsFinalCommit

--- my-plant-app/app/Http/Controllers/PlantController.php
+++ my-plant-app/app/Http/Controllers/PlantController.php
@@ -12,7 +12,8 @@ class PlantController extends Controller
      */
     public function index()
     {
-        //
+        $plants = Plant::all();
+        return view ('plants.index', compact('plants'));
     }
 
     /**
--- my-plant-app/routes/web.php
+++ my-plant-app/routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Contollers\PlantController;
+use App\Http\Controllers\PlantController;
 
 Route::get('/', function () {
     return view('welcome');
