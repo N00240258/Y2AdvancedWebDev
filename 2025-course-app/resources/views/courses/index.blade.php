6bcf7e0e73682b2ae62a87f224a1312b039a1b99
N00240258 <N00240258@iadt.ie>
2025-09-30 17:12:04 +0100

    lesson 7 finished but courses does not show

--- 2025-course-app/resources/views/courses/index.blade.php
+++ 2025-course-app/resources/views/courses/index.blade.php
@@ -1,3 +1,28 @@
-<div>
-    <!-- Simplicity is the consequence of refined emotions. - Jean D'Alembert -->
-</div>
+<x-app-layour>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __("All Courses")}}
+        </h2>
+
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900">
+                    <h3 class="font-semibold text-lg mb-4"> List of Courses:</h3>
+                    <div class="grid grid-cols-1 sm:grid-cols2 lg:grid-cols-3 gap-6">
+                        @foreach($courses as $course)
+                        <a href="{{route("courses.show", $course) }}">
+                            <x-course-card
+                                :title="$course->title"
+                                :image="$course->image"
+                            />
+                        </a>
+                        @endforeach
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layour>
--- 2025-course-app/routes/web.php
+++ 2025-course-app/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\CourseController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -13,8 +14,15 @@ Route::get('/dashboard', function () {
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::get('/courses', [CourseController::class, 'index'])->name('courses.index');
+    Route::get('/courses/create', [CourseController::class, 'create'])->name('courses.create');
+    Route::get('/courses/{course}', [CourseController::class, 'show'])->name('courses.show');
+    Route::post('/courses', [CourseController::class, 'store'])->name('courses.store');
+
+    Route::get('/courses/{course}/edit', [CourseController::class, 'edit'])->name('courses.edit');
+    Route::put('/courses/{course}', [CourseController::class, 'update'])->name('courses.update');
+    Route::delete('/courses/{course}', [CourseController::class, 'destroy'])->name('courses.destroy');
 });
 
 require __DIR__.'/auth.php';
