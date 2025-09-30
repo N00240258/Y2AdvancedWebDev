d9e30b7a6cf7997707df36eb1377903752b745f4
N00240258 <N00240258@iadt.ie>
2025-09-30 16:37:36 +0100

    ca1lesson6finish

---
+++ 2025-course-app/database/seeders/CourseSeeder.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class CourseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Course::insert([
+            [
+                'courseCode' => "DL836",
+                'title' => "Creative Computing",
+                'description' => "Apply and develop your creativity in the thriving and dynamic area of computing. Gain the hands-on skills and knowledge to create solutions for today’s fast-paced world. Learn core computing skills such as programming, app development and databases, while keeping a focus on the design of the technology interaction with the user.",
+                'points' => "210",
+                'years' => "4",
+                'image' => "CreativeComputing.jpg"
+            ],
+            [
+                'courseCode' => "DL847",
+                'title' => "3D Animation",
+                'description' => "Do you want to create imaginary and realistic environments and characters and bring them to life? Learn to design, build and create digital worlds and tell the stories of tomorrow using advanced technologies.",
+                'points' => "873",
+                'years' => "4",
+                'image' => "3DAnimation.jpg"
+            ],
+            [
+                'courseCode' => "DL864",
+                'title' => "Film Studies",
+                'description' => "This Film Studies degree for people who want to think critically about film and write it – combining academic depth with practical creativity and opening a pathway to professional screenwriting.",
+                'points' => "0",
+                'years' => "3",
+                'image' => "FilmStudies.jpg"
+            ]
+        ]);
+    }
+}
